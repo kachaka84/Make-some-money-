2a5bef17fd47dbddf04b2ec9ee055ec8248da6e2
kachaka84 <hristovachev@abv.bg>
2025-04-14 19:43:08 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Прогнозаторът на Вачев</h1>
+      <p>Тук скоро ще се появяват прогнозите за мачове!</p>
+    </div>
+  );
+}
+
+export default App;
